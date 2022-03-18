95403b115902df164a1ffb1b0426955a9a62bc44
francisamidu <francisamidu124@gmail.com>
2022-03-19 00:18:08 +0200

    Prepping for deployment

--- contexts/ContractContext.tsx
+++ contexts/ContractContext.tsx
@@ -17,11 +17,9 @@ const ContractProvider = ({
   const contractABI = require("../artifacts/contracts/Note.sol/Note.json");
   useEffect(() => {
     //Blockchain config
-    // const contractAddress = "0x2104Df21CAF52550d3d1eC3E2680319338f8E10f";
-    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const contractAddress = `${process.env.CONTRACT_ADDRESS}`;
     const provider = new ethers.providers.JsonRpcProvider(
-      "http://127.0.0.1:8545/"
-      // `https://ropsten.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
+      `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`
     );
     provider.listAccounts().then((accs) => setAccounts(accs));
     const signer = provider.getSigner();
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,8 @@ module.exports = {
       chainId: 1337,
     },
     ropsten: {
-      url: `https://ropsten.infura.io/v3/f8a9c7c9680045a78680e5988d5edc3e`,
-      accounts: [
-        `a6bb3a858c0e6f259396f254f95d934015934fbc3f4e111f1800afeed3148a9c`,
-      ],
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [`${process.env.PRIVATE_KEY}}`],
     },
   },
 };
